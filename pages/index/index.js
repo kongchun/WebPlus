b56fdb87c29ea42f28452ac88e84781d669656d4
冉小叶 <437548960@qq.com>
2018-06-20 13:27:23 +0800

    主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,11 @@ const app = getApp()
 
 Page({
   data: {
+    system: { windowHeight: 603, windowWidth:373}
   },
   onLoad: function () {
+    wx.pro.getSystemInfo().then(res=>{
+      this.setData({ system: res });
+    });
   }
 })
